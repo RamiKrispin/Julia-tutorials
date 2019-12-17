3a2048d721a9f695d19bb0c6a0be5b4730fbe095
RamiKrispin <ramkrisp@umich.edu>
2019-12-17 04:14:36 -0800

    Functions summary

--- basic operation.jl
+++ basic operation.jl
@@ -98,3 +98,111 @@ numbers
 
 # rand function create a random set of arrays
 rand(4, 3)
+
+
+# Loops
+#------------------------------
+# while loop
+n = 0
+while n < 10
+    n += 1
+    println(n)
+end
+
+# for loop
+for n in 1:10
+    println(n)
+end
+
+# Example for nested for loop
+# creating a 5X5 zeros matrix
+# the zeroes function create a matrix of zeros
+m, n = 5 , 5
+
+# Method A
+A = zeros(m, n)
+A
+
+for i in 1:m
+    for j in 1:n
+        A[i, j] = i + j
+    end
+end
+
+A
+
+# Method B
+B = zeros(m, n)
+for i in 1:m, j in 1:n
+    B[i, j] = i + j
+end
+
+B
+
+# Method C
+
+C = [i + j for i in 1:m, j in 1:n]
+C
+
+# Conditionals
+# if/else
+x = 10
+y = 5
+
+if x > y
+    x
+else
+    y
+end
+
+# ternary operator
+(x > y) ? x : y
+
+(x > y) && println("$x is larger than $y")
+(x < y) && println("$x is smaller than $y")
+
+# Functions
+# Declearing function
+function f(x)
+    x^2
+end
+
+f(2)
+
+# Alternatively, can declare the function as follow
+
+f2(x) = x^2
+
+f2(2)
+
+# Anonymous function
+f3 = x -> x^2
+f3(2)
+
+# Duck-typing in Julia
+# Julia function will work on any type of input as long as it make sense
+
+A = rand(3,3)
+A
+f(A)
+f3(A)
+
+# Mutating vs non-mutating functions
+# the different between functions followed by ! and without
+
+v = [2, 3, 1]
+v
+sort(v)
+# the content (or the order of v) did not changed
+v
+
+sort!(v)
+v
+
+# Broadcasting
+# By placing . between any function name and its argument list,
+# we tell that function to broadcast over the elemnts of the input objects
+
+A = [i + 3 * j for j in 0:2, i in 1:3]
+f(A)
+B = f.(A)
